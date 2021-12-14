c590ba4e814f429f81b4a8ece5600935bafa7b8d
Fiona Henry <fhenry@g.harvard.edu>
2021-12-14 15:44:25 -0500

    bug fixes in comparison code

--- compare_rebound_odeint.py
+++ compare_rebound_odeint.py
@@ -1,6 +1,8 @@
 import rebound
 import numpy as np
 import nbody as nb
+import importlib
+importlib.reload(nb)
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
@@ -9,15 +11,17 @@ times = np.arange(0, sim_duration)
 
 def rebound_sim():
   sim = rebound.Simulation()
-  sim.units = ("AU", "kg", "d")
+  sim.units = ("AU", "kg", "day")
   sim.add(["Sun", "Earth", "Mars"], date="2018-01-01 00:00")
 
   sim.move_to_com()
-  bodies = [{}] * len(sim.particles)
+  bodies = []
   for i, particle in enumerate(sim.particles):
-    bodies[i]["m"] = particle.m
-    bodies[i]["p"] = (particle.x, particle.y, particle.z)
-    bodies[i]["v"] = (particle.vx, particle.vy, particle.vz)
+    temp = {}
+    temp["m"] = particle.m
+    temp["p"] = (particle.x, particle.y, particle.z)
+    temp["v"] = (particle.vx, particle.vy, particle.vz)
+    bodies.append(temp)
 
   N = len(times)
   pos = np.zeros((len(sim.particles), N, 3))
@@ -29,13 +33,13 @@ def rebound_sim():
       p = sim.particles[j]
       pos[j, i, :] = np.array([p.x, p.y, p.z])
 
-  return bodies, pos
+  return bodies, pos, sim.G
 
 def plot_error(i, title, solution, pos, t):
     fig = plt.figure(figsize=(8, 6))
     errors = []
     num_bodies = 3
-    for k in range(730):
+    for k in t:
       day_sol = np.array([solution[k, i] - solution[k, 0], solution[k, num_bodies + i] - solution[k, num_bodies],
                           solution[k, num_bodies * 2 + i] - solution[k, num_bodies * 2]])
       errors.append(np.linalg.norm(pos[i, k] - day_sol))
@@ -45,7 +49,8 @@ def plot_error(i, title, solution, pos, t):
     plt.xlabel("Days")
 
 if __name__ == "__main__":
-  init, pos = rebound_sim()
+  init, pos, G = rebound_sim()
+  num_bodies = len(init)
 
   x0 = [o['p'][0] for o in init]
   y0 = [o['p'][1] for o in init]
@@ -55,7 +60,8 @@ if __name__ == "__main__":
   vz0 = [o['v'][2] for o in init]
   s0 = np.concatenate((x0, y0, z0, vx0, vy0, vz0))
 
-  solution = odeint(nb.F, s0, times)
+
+  solution = odeint(nb.F, s0, times, args=(G,))
 
 
   plot_error(1, "earth.png", solution, pos, times)
--- nbody.py
+++ nbody.py
@@ -4,31 +4,8 @@ from scipy.integrate import odeint
 from mpl_toolkits import mplot3d
 from matplotlib.animation import FuncAnimation
 
-softening_param = 0
-#km, kg, s
-sun = {
-    "m": 1.9884754159566474e+30,
-    "p": (130.12022433639504, 493.88921553478576, 1.9894885155190423),
-    "v": (-9.08252417469984e-05, 2.763470714263038e-05, 9.333023252795563e-08)
-}
-earth = {
-    "m": 6.045825576341311e+24,
-    "p": (-52532397.16477036, -142290670.7988091, 6714.9380375893525),
-    "v": (27.459556210605758, -10.428715463342813, 0.0004283693350210454)
-}
 
-mars = {
-    "m": 6.417120534329241e+23,
-    "p": (91724696.20692892, -189839018.6923888, -6228099.232650615),
-    "v": (22.733051422552098, 12.621328917003236, -0.29323856116219776)
-}
-
-init = [sun,earth,mars]
-
-G = 6.67408e-20
-num_bodies = len(init)
-
-def x_accel(s,i,j):
+def x_accel(s,i,j, num_bodies):
     m2 = init[j]['m']
     x1 = s[i]
     y1 = s[i+num_bodies]
@@ -36,10 +13,10 @@ def x_accel(s,i,j):
     x2 = s[j]
     y2 = s[j+num_bodies]
     z2 = s[j+num_bodies*2]
-    r = np.linalg.norm(np.array([x2, y2, z2]) - np.array([x1, y1, z1])) + softening_param**2
+    r = np.linalg.norm(np.array([x2, y2, z2]) - np.array([x1, y1, z1]))
     return m2*(x2-x1)/(r)**(3)
 
-def y_accel(s,i,j):
+def y_accel(s,i,j,num_bodies):
     m2 = init[j]['m']
     x1 = s[i]
     y1 = s[i+num_bodies]
@@ -47,11 +24,11 @@ def y_accel(s,i,j):
     x2 = s[j]
     y2 = s[j+num_bodies]
     z2 = s[j+num_bodies*2]
-    r = np.linalg.norm(np.array([x2, y2, z2]) - np.array([x1, y1, z1])) + softening_param**2
+    r = np.linalg.norm(np.array([x2, y2, z2]) - np.array([x1, y1, z1]))
     return m2*(y2-y1)/(r)**(3)
 
 
-def z_accel(s,i,j):
+def z_accel(s,i,j,num_bodies):
     m2 = init[j]['m']
     x1 = s[i]
     y1 = s[i+num_bodies]
@@ -59,30 +36,31 @@ def z_accel(s,i,j):
     x2 = s[j]
     y2 = s[j+num_bodies]
     z2 = s[j+num_bodies*2]
-    r = np.linalg.norm(np.array([x2, y2, z2]) - np.array([x1, y1, z1])) + softening_param**2
+    r = np.linalg.norm(np.array([x2, y2, z2]) - np.array([x1, y1, z1]))
     return m2*(z2-z1)/(r)**(3)
 
-def xpp(s,i):
+def xpp(s,i, num_bodies, G):
     other_indices = list(range(num_bodies))
     other_indices.remove(i)
-    return G*sum([x_accel(s,i,j) for j in other_indices])
+    return G*sum([x_accel(s,i,j,num_bodies) for j in other_indices])
 
-def ypp(s,i):
+def ypp(s,i, num_bodies, G):
     other_indices = list(range(num_bodies))
     other_indices.remove(i)
-    return G*sum([y_accel(s,i,j) for j in other_indices])
+    return G*sum([y_accel(s,i,j,num_bodies) for j in other_indices])
 
-def zpp(s,i):
+def zpp(s,i, num_bodies, G):
     other_indices = list(range(num_bodies))
     other_indices.remove(i)
-    return G*sum([z_accel(s,i,j) for j in other_indices])
+    return G*sum([z_accel(s,i,j,num_bodies) for j in other_indices])
 
-def F(s,t):
+def F(s,t, G=1):
+    num_bodies = int(s.shape[0]/6)
     return np.concatenate(
         (s[3*num_bodies:4*num_bodies],s[4*num_bodies:5*num_bodies],s[5*num_bodies:6*num_bodies],
-         [xpp(s,i) for i in range(num_bodies)],
-         [ypp(s,i) for i in range(num_bodies)],
-         [zpp(s,i) for i in range(num_bodies)]))
+         [xpp(s,i, num_bodies, G) for i in range(num_bodies)],
+         [ypp(s,i, num_bodies, G) for i in range(num_bodies)],
+         [zpp(s,i,num_bodies, G) for i in range(num_bodies)]))
 
 def plot_3d(solution, k):
     fig = plt.figure(figsize=(8, 8))
@@ -104,18 +82,6 @@ def plot_3d(solution, k):
     plt.axis('off')
     plt.savefig("odeint.png", bbox_inches='tight')
 
-def plot_2d():
-    fig = plt.figure(figsize=(6, 6))
-    while True:
-        for k in range(1000):
-            plt.cla()
-            for i in range(num_bodies):
-                plt.plot(solution[0:k,i]- solution[0:k,0], solution[0:k,num_bodies+i] - solution[0:k,num_bodies], color="black", ls="--")
-                plt.scatter(solution[k, i] - solution[k, 0], solution[k, num_bodies + i]- solution[k, num_bodies], alpha=.5)
-            plt.xlim([-3e8, 3e8])
-            plt.ylim([-3e8, 3e8])
-            fig.canvas.draw()
-            plt.pause(.0001)
 
 def animate_outcome(pos, colors, sizes):
     fig = plt.figure()
@@ -142,7 +108,7 @@ def animate_outcome(pos, colors, sizes):
         ax.set_xlim(-190289032.31830737, 227205650.0355562)
         ax.set_zlim(-7199082.133277591, 4000949.6426398293)
 
-
+        num_bodies = int(pos.shape[1]/6)
         for p in range(num_bodies):
             ax.plot3D(pos[0:i, p] - pos[0:i, 0], pos[0:i, num_bodies + p] - pos[0:i, num_bodies],
                       pos[0:i, num_bodies * 2 + p] - pos[0:i, num_bodies * 2], color="white", ls="--")
@@ -153,16 +119,39 @@ def animate_outcome(pos, colors, sizes):
 
 
 if __name__ == "__main__":
-    x0 = [o['p'][0] for o in init]
-    y0 = [o['p'][1] for o in init]
-    z0 = [o['p'][2] for o in init]
-    vx0 = [o['v'][0] for o in init]
-    vy0 = [o['v'][1] for o in init]
-    vz0 = [o['v'][2] for o in init]
-    s0 = np.concatenate((x0,y0,z0,vx0,vy0,vz0))
 
-    t = np.linspace(0,3600*24*365*2,79)
-    solution = odeint(F,s0,t)
-    anim = animate_outcome(solution, ["yellow", "blue" , "red"], [30,20,15])
-    plt.show()
-    #anim.save("nbody-odeint.gif", writer="imagemagick", fps=2)
+    #km, kg, s
+  sun = {
+      "m": 1.9884754159566474e+30,
+      "p": (130.12022433639504, 493.88921553478576, 1.9894885155190423),
+      "v": (-9.08252417469984e-05, 2.763470714263038e-05, 9.333023252795563e-08)
+  }
+  earth = {
+      "m": 6.045825576341311e+24,
+      "p": (-52532397.16477036, -142290670.7988091, 6714.9380375893525),
+      "v": (27.459556210605758, -10.428715463342813, 0.0004283693350210454)
+  }
+
+  mars = {
+      "m": 6.417120534329241e+23,
+      "p": (91724696.20692892, -189839018.6923888, -6228099.232650615),
+      "v": (22.733051422552098, 12.621328917003236, -0.29323856116219776)
+  }
+
+  init = [sun,earth,mars]
+
+  G = 6.67408e-20
+
+  x0 = [o['p'][0] for o in init]
+  y0 = [o['p'][1] for o in init]
+  z0 = [o['p'][2] for o in init]
+  vx0 = [o['v'][0] for o in init]
+  vy0 = [o['v'][1] for o in init]
+  vz0 = [o['v'][2] for o in init]
+  s0 = np.concatenate((x0,y0,z0,vx0,vy0,vz0))
+
+  t = np.linspace(0,3600*24*365*2,79)
+  solution = odeint(F,s0,t, args=(G,))
+  anim = animate_outcome(solution, ["yellow", "blue" , "red"], [30,20,15])
+  plt.show()
+  #anim.save("nbody-odeint.gif", writer="imagemagick", fps=2)
