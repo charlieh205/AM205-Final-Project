67c928641538302760d359bbf0562985f5342854
Charlie <charliesharrington@gmail.com>
2021-12-18 08:40:40 -0700

    Our porkchop now matches poliastro

--- our_porkchop.py
+++ our_porkchop.py
@@ -10,7 +10,7 @@ class Porkchop(object):
         self,
         launch_span,
         arrival_span,
-        max_c3 = 1000.0  * u.km**2 / u.s**2,
+        max_c3 = 45.0 * u.km**2 / u.s**2,
         ax = None
     ):
         self.launch_span = launch_span
@@ -24,23 +24,22 @@ class Porkchop(object):
         c3 = 1000 * np.ones((len(self.launch_span), len(self.arrival_span)))
         for i in range(len(self.launch_span)):
             launch = self.launch_span[i].to_datetime()
-            print(f"{i}/{len(self.launch_span)}")
+            print(f"{i+1}/{len(self.launch_span)}")
             for j in range(len(self.arrival_span)):
                 arrival = self.arrival_span[j].to_datetime()
                 duration_days = (arrival - launch).days
-                if duration_days > 40 and duration_days < 365:
+                if duration_days > 30:
                     dv_dpt, _ = earth_to_mars(launch, arrival)
                     dv_dpt *= 1.496e8
                     dv_dpt = dv_dpt * (u.km / u.s)
                     dv_dpt = dv_dpt.to(u.m / u.s)
                     dv_dpt = norm(dv_dpt)
                     c3_launch = dv_dpt**2
-                    c3[i, j] = c3_launch.to_value(u.km**2 / u.s**2)
+                    c3[j, i] = c3_launch.to_value(u.km**2 / u.s**2)
         return c3
 
     def plotter(self, filename, num_best=10, plot_best=False):
         c3_launch = self._get_c3_launch()
-        # print(c3_launch)
         
         if self.ax is None:
             fig, self.ax = plt.subplots(figsize=(15, 15))
@@ -77,18 +76,14 @@ class Porkchop(object):
 
         c3_launch = c3_launch * u.km**2 / u.s**2
 
-        c3_launch_arr = np.array(c3_launch)
-
-        (l, a) = np.unravel_index(np.argmin(c3_launch, axis=None), c3_launch.shape)
+        (l, a) = np.unravel_index(np.argmin(np.transpose(c3_launch), axis=None), c3_launch.shape)
 
         best_launch = self.launch_dates[l]
         best_arrival = self.arrival_dates[a]
-        print(best_launch)
-        print(best_arrival)
 
         if plot_best:
             lab = f"Launch date: {best_launch}\nArrival date: {best_arrival}"
-            self.ax.scatter(best_launch, best_arrival, color="red", label=lab)
+            self.ax.scatter([best_launch], [best_arrival], color="red", label=lab)
             self.ax.legend(loc="best")
         
         plt.savefig(f"ours_{filename}.svg", bbox_inches="tight")
